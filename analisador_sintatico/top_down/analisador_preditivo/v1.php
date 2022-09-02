2222942f7c6022234b09c63e2cb445651a48d141
Roni Deringer Aviz <roni.aviz@IFRSL.redelocal>
2022-09-02 09:17:42 -0300

    novo codigo de analise preditiva

---
+++ analisador_sintatico/top_down/analisador_preditivo/v1.php
@@ -0,0 +1,19 @@
+<?php
+
+/***
+ * proximo token -> aponta para a proxima entrada
+ * define topo da pilha como simbilo inicial da gramatica
+ */
+while(nao terminarl de ler entrada){
+if(topo == token){
+ //desemepilha, avança token
+}elseif(topo nao terminal){
+    busca na tabela de ação
+    m[topo, token]
+    desemepilha topo
+    empilha producao
+
+}
+
+}
+}
